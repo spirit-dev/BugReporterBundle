a33a15d19abe8f7c397f7241f076cb342790578b
Jean Bordat <bordat.jean@gmail.com>
2015-07-09 18:54:03 +0200

    update according to JQuery loading
    Signed-off-by: Jean Bordat <bordat.jean@gmail.com>

--- Resources/public/js/bug-report.js
+++ Resources/public/js/bug-report.js
@@ -1,47 +1,50 @@
-$(document).ready(function () {
+setTimeout(function() {
+    console.log('Bug reporter Ready!');
+    $(document).ready(function () {
 
-    $('#bug-report-form')
-        .on('submit', function (e) {
-            e.preventDefault();
-        })
-        .on('valid.fndtn.abide', function () {
+        $('#bug-report-form')
+            .on('submit', function (e) {
+                e.preventDefault();
+            })
+            .on('valid.fndtn.abide', function () {
 
-            var $this = $(this);
+                var $this = $(this);
 
-            var al = new gifLoader(".bug-reporter");
+                var al = new gifLoader(".bug-reporter");
 
-            $.ajax({
-                url: $this.attr('action'),
-                type: $this.attr('method'),
-                data: $this.serialize(),
-                success: function (data) {
-                    console.log(data);
-                    bugReportSuccessDisplay();
-                },
-                error: function (data) {
-                    console.log(data)
-                    bugReportErrorDisplay();
-                },
-                complete: function () {
-                    al.remove();
-                }
+                $.ajax({
+                    url: $this.attr('action'),
+                    type: $this.attr('method'),
+                    data: $this.serialize(),
+                    success: function (data) {
+                        console.log(data);
+                        bugReportSuccessDisplay();
+                    },
+                    error: function (data) {
+                        console.log(data)
+                        bugReportErrorDisplay();
+                    },
+                    complete: function () {
+                        al.remove();
+                    }
+                });
             });
-        });
 
-});
+    });
 
-function bugReportErrorDisplay() {
-    $('#report-issue').html('<div id="report-removal" class="alert-box alert">Sorry an, error occured! You may retry.</div>');
-    setTimeout(function () {
-        $('#report-removal').remove();
-    }, 5000);
-}
+    function bugReportErrorDisplay() {
+        $('#report-issue').html('<div id="report-removal" class="alert-box alert">Sorry an, error occured! You may retry.</div>');
+        setTimeout(function () {
+            $('#report-removal').remove();
+        }, 5000);
+    }
 
-function bugReportSuccessDisplay() {
-    $('#report-issue').html('<div id="report-removal" class="alert-box success">Your bug has been well reported! We will manage it soon.</div>');
-    //setTimeout(function () {
-    //    $('#report-removal').remove();
-    //    $('.bug-reporter a').click();
-    //    $('.small-input').val('');
-    //}, 5000);
-}
+    function bugReportSuccessDisplay() {
+        $('#report-issue').html('<div id="report-removal" class="alert-box success">Your bug has been well reported! We will manage it soon.</div>');
+        //setTimeout(function () {
+        //    $('#report-removal').remove();
+        //    $('.bug-reporter a').click();
+        //    $('.small-input').val('');
+        //}, 5000);
+    }
+}, 1000);
